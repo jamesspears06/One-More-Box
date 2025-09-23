f1f0c76cb159cd1ac2a16fd6a7fdd8386cc62cf8
Ju-im <joshua2.imarhiagbe@live.uwe.ac.uk>
2025-09-23 22:17:06 +0100

    changes
    changes to project settings and main logic is now handled by the level itself

--- Scripts/reset.gd
+++ Scripts/reset.gd
@@ -1,12 +1,10 @@
 extends Control
 
-@export var  self_scene:PackedScene
 func _ready() -> void:
-	self_scene = preload("res://Scenes/levels/level_2.tscn")
 	size = get_viewport_rect().size
 
 
 
 func _on_button_button_down() -> void:
-	get_tree().change_scene_to_packed(self_scene)
+	get_tree().reload_current_scene()
 	
